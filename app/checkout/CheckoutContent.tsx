d443bbd616bdcd5321baf5b8e905c8b3a851bf76
ok <ok@oks-MacBook-Air.local>
2024-09-21 14:25:09 +0700

    patch

---
+++ app/checkout/CheckoutContent.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { useSearchParams } from 'next/navigation'
+
+export default function CheckoutContent() {
+  const searchParams = useSearchParams()
+  
+  // Use searchParams here
+  
+  return (
+    <div>
+      {/* Your checkout page content */}
+    </div>
+  )
+}
